eaf2203339e3e8854661bb66f1cf29dd18b27313
yangm2 <yangm2@users.noreply.github.com>
2026-02-17 20:05:00 -0800

    TODO ToolMessage capture

--- backend/scripts/langsmith_evaluators.py
+++ backend/scripts/langsmith_evaluators.py
@@ -33,8 +33,8 @@ INPUT_OUTPUT: Final = dedent(
     </input>
 
     Use the Model-Under-Test Output below to evaluate the response.  Disregard
-    the Model-Under-Test Reasoning & Tools Used sections, which are for
-    evaluation debugging only and should not affect the scores.
+    other Model-Under-Test sections, which are for evaluation debugging only
+    and should not affect the scores.
     
     <output>
     {outputs}
